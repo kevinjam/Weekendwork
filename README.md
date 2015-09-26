22bee5ae3b2a14c5a5c9439a3f761d8182ebb49a
kevinjan <kevin.janvier5@gmail.com>
2015-09-26 17:09:39 +0300

    insert and view using php

---
+++ README.md
@@ -0,0 +1,12 @@
+PHP_2nd-Year/ Class 1 and 2
+===========================
+
+PHP Class Lab Pratice 
+ST.lawrence University 
+Faculty of ICT 
+Lecturer: Mr.John Bosco Lubega
+Tutor:    Kevin Janvier Chinabalire
+
+Database Name: student_reg.sql
+
+N.B: You can Modify the root username and Password which correspond with your Server
---
+++ view.php
@@ -0,0 +1,24 @@
+<title>St.lawrence University | View</title>
+
+<?php
+$db=mysql_connect("localhost", "root", "")or die("cannot connect to the Server !!!!Sorry .. try again later"); 
+mysql_select_db("student_reg")or die("cannot select Database");
+?>
+<?php
+$sql= mysql_query("select * from name_tbl");
+echo'<table width="43%" border="1" align="center">
+  <tr>
+  <h1 align="center">View All (Student_reg) Database </h1><a href="index.php">Back to Main</a>
+   <td width="14%"><strong>Id</strong></td>
+    <td width="43%"><strong>First Name</strong></td>
+    <td width="43%"><strong>Second Name</strong></td>
+  </tr>'; 
+	 while($hello=mysql_fetch_array($sql)){
+	 echo'
+  <tr>
+    <td>'.$hello[Id].'</td>
+    <td>'.$hello[Fname ].'</td>
+    <td>'.$hello[Lname ].'</td>
+  </tr>';}
+  echo'</table>';
+?>
